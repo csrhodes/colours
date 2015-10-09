ceccbe38609144fb385ed1d82b612be67e5eb005
Christophe Rhodes <csr21@cantab.net>
2015-10-09 12:40:34 +0100

    separate canonical and canonical-or-lose functions

--- spaces.lisp
+++ spaces.lisp
@@ -1,24 +1,66 @@
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (make-package "COLOURS.SPACES" :use '("CL")))
+(defpackage "COLOURS.SPACES"
+  (:use "CL")
+  (:export "CANONICAL" "CANONICAL-OR-LOSE" "DEFINE-COLOUR-SPACE"
+           "RGB" "rgb"
+           "HSB" "hsb"
+           "XYZ" "xyz"
+           "xyY"
+           "sRGB"
+           "sRGB/linear"
+           "CIELAB" "cielab"))
 (in-package "COLOURS.SPACES")
 (export (intern "CANONICAL"))
 
 (defvar *canonical-spaces* nil)
-(defmacro define-colour-space (canonical &rest aliases)
+(defmacro define-colour-space (canonical aliases &body body &aux doc)
+  (when (and body (stringp (car body)))
+    (setq doc (car body) body (cdr body)))
   `(progn
+     (setq *canonical-spaces* (set-difference *canonical-spaces* ',aliases))
+     (remprop ',canonical 'canonical)
+     (remprop ',canonical 'doc)
+     ;; this is wrong.  (I think it would probably still be wrong if
+     ;; we made it export from "COLOURS.SPACES" always)
      (export (cons ',canonical ',aliases))
      (pushnew ',canonical *canonical-spaces*)
-     ,@(loop for a in aliases collect `(setf (get ',a 'canonical) ',canonical))))
+     ,@(loop for a in aliases collect `(setf (get ',a 'canonical) ',canonical))
+     ,@(when doc
+         `((setf (get ',canonical 'doc) ,doc)))))
 
 (defun canonical (id)
   (let ((c (or (get id 'canonical) id)))
-    (unless (member c *canonical-spaces*)
-      (error "not a colour space: ~S" c))
+    (when (member c *canonical-spaces*)
+      c)))
+
+(defun canonical-or-lose (id)
+  (let ((c (canonical id)))
+    (unless c
+      (error "not a colour space: ~S" id))
     c))
 
-(define-colour-space RGB rgb)
-(define-colour-space HSB hsb)
-(define-colour-space XYZ xyz)
-(define-colour-space xyY)
-(define-colour-space sRGB)
-(define-colour-space CIELAB cielab)
+(defmethod documentation ((o symbol) (doc-type (eql 'space)))
+  (let ((c (canonical o)))
+    (when c
+      (get c 'doc))))
+
+(define-colour-space RGB (rgb)
+  "Red-Green-Blue cube, with values in [0,255]³")
+(define-colour-space HSB (hsb)
+  "Hue-Saturation-Brightness cone, with values:
+- Hue ∈ [0,2π)
+- Saturation ∈ [0,1]
+- Brightness ∈ [0,255]")
+(define-colour-space XYZ (xyz)
+  "The CIE (1931) Standard Observer colour space.")
+(define-colour-space xyY ()
+  "Chromaticity-Luminance transformation of the CIE XYZ colour space,
+with values:
+- x,y ∈ [0,1]² ∧ x+y ≤ 1
+- Y ∈ [0,∞) (as CIE XYZ)")
+(define-colour-space sRGB ()
+  "A standard default colour space for the Internet (IEC 61966-2-1).
+Values in [0,1]³ correspond to RGB values in [0,255]³")
+(define-colour-space sRGB/linear ()
+  "The CIE (1931) Standard Observer colour space, expressed in terms
+of the sRGB primaries.")
+(define-colour-space CIELAB (cielab))
